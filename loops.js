c06fd024c5fcab50f27285575bb39f1edd169839
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:26:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(Array) {
 for (let i=0; i<25; i++)
 if (i==1){
-  arry.push("I am 1 stange loop")
+  array.push("I am 1 stange loop")
 } else {
-  console.log ("I am" +i+ "strange loop.");
+  array.push("I am ")
 }
   
 }
