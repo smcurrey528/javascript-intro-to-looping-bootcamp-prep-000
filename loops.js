17e700ce5439f96e650cc27aff73c43dfb4c1c50
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:17:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,3 +2,15 @@ for ([initialization];
 [condition]; [iteration]) {
   [loopBody]
 }
+// i is set equal to 1
+// as long as i is less than 100 execute the code in the loopBody
+// - which is print "Hello World"; increment i each time the code in loopBody is executed
+ 
+for (var i = 1; i < 100; i++) {
+  console.log( "Hello World the " + i + " time" );
+}
+ 
+// The above prints:
+// Hello World the 1 time
+// Hello World the 2 time
+// Hello World the 3 time
