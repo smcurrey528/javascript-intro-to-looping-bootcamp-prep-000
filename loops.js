098d796fec0be167baf2cef919f0cf4a637cd526
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:27:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,9 @@
 function forLoop(Array) {
-for (let i=i; i<25; i++)
-  if (i==0){
+for (let i=i; i<25; i++) {
+  if (i==0) {
   array.push("I am 1 stange loop")
 } else {
   array.push("I am ${i} strange loops.")
    }
   }
+}
