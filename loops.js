97a3e7a4c0a6125cf2debffcb18683fff78daaf8
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:36:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,7 +10,7 @@ function forLoop(array) {
   return array;
 }
 function whileLoop (number)
-while (number>0 {
+while (number>0) {
   console.log ("And I ran; I ran so far away!");
 }
 return "done";
