b8e9066b8b2b4f0ec1fca99812522a58a719b9d6
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:24:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,4 @@
-var gifts = ['teddy bear', 'drone', 'doll']
- 
-function wrapGift(gift) {
-  console.log(`Wrapped ${gift} and added a bow!`)
-}
-
-for ([initialization];
-[condition]; [iteration]) {
-  [loopBody]
-}
+Var
 for (let i=1; i<25; i++)
 {
   console.log ("I am" +i+ "strange loop.");
