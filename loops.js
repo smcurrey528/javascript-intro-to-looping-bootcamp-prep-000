2c77d04a1941040f7be346336ca4e7c392d3c94d
Sarah Currey <curreysarah28@gmail.com>
2018-05-03 23:39:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,5 +16,11 @@ while (number > 0) {
 return "done";
 }
 function doWhileLoop (array) {
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 }
